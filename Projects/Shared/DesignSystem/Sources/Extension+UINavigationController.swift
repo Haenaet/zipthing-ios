f765a73c78a0c5a2e4bc770563b93c51928a3356
Hongsik Choi <playhong16@gmail.com>
2024-01-18 13:22:41 +0900

    feat: 네비게이션 바의 타이틀 폰트 및 글자색 설정 추가

--- Projects/Shared/DesignSystem/Sources/Extension+UINavigationController.swift
+++ Projects/Shared/DesignSystem/Sources/Extension+UINavigationController.swift
@@ -12,7 +12,10 @@ extension UINavigationController {
     func setBarAppearance() {
         let appearance: UINavigationBarAppearance = {
             let appearance = UINavigationBarAppearance()
+            // 네비게이션 바의 배경색 설정
             appearance.backgroundColor = .systemYellow
+            // 네비게이션 바의 타이틀 폰트 및 글자색 설정
+            appearance.titleTextAttributes = [.font: UIFont.boldSystemFont(ofSize: 20.0), .foregroundColor: UIColor.black]
             return appearance
         }()
         
