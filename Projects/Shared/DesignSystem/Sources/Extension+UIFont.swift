e62224e77d9578428f00e4f2bfdb98d37a2ab36a
Hongsik Choi <playhong16@gmail.com>
2024-01-18 13:24:12 +0900

    create: UIFont 확장 파일 생성

---
+++ Projects/Shared/DesignSystem/Sources/Extension+UIFont.swift
@@ -0,0 +1,13 @@
+//
+//  Extension+UIFont.swift
+//  DesignSystem
+//
+//  Created by playhong on 2024/01/18.
+//  Copyright © 2024 Zipthing. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+}
