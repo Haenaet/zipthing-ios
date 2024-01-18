9e6d54a982c39f5c5c07f7090c4134ae259f2661
Hongsik Choi <playhong16@gmail.com>
2024-01-18 13:06:42 +0900

    feat: 한 번에 여러개의 뷰를 하위에 추가하는 메서드 구현

---
+++ Projects/Shared/DesignSystem/Sources/Extension+UIView.swift
@@ -0,0 +1,15 @@
+//
+//  Extension+UIView.swift
+//  DesignSystem
+//
+//  Created by playhong on 2024/01/18.
+//  Copyright © 2024 Zipthing. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        for view in views { addSubview(view) }
+    }
+}
