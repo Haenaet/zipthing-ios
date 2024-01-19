450ab43c851a1591fac0f09bb7af33896a0e8e3a
seryeong <seryz029@gmail.com>
2024-01-19 22:26:22 +0900

    setLineHeight 기능구현

--- Projects/Shared/DesignSystem/Sources/Extension/Extension+UILabel.swift
+++ Projects/Shared/DesignSystem/Sources/Extension/Extension+UILabel.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2024 Zipthing. All rights reserved.
 //
 
-import Foundation
+import UIKit.UILabel
+
+extension UILabel {
+    func setLineHeight(_ lineHeight: CGFloat) {
+        guard let text = text else {
+            return
+        }
+        
+        let style = NSMutableParagraphStyle()
+        style.maximumLineHeight = lineHeight
+        style.minimumLineHeight = lineHeight
+        
+        let attributes: [NSAttributedString.Key : Any] = [
+            .paragraphStyle : style,
+            .font : self.font as Any,
+            .foregroundColor : self.textColor as Any,
+            .baselineOffset : (lineHeight - self.font.lineHeight) / 4
+        ]
+        
+        let attributeString = NSAttributedString(string: text,
+                                                 attributes: attributes)
+        
+        self.attributedText = attributeString
+    }
+}
