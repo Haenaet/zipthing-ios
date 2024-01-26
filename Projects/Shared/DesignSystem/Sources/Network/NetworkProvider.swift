822251b6a9a2b99337fddd0b959f87de6f251c72
Hongsik Choi <playhong16@gmail.com>
2024-01-26 23:29:40 +0900

    create: NetworkProvider.swift 생성

---
+++ Projects/Shared/DesignSystem/Sources/Network/NetworkProvider.swift
@@ -0,0 +1,13 @@
+//
+// NetworkProvider.swift
+//  DesignSystem
+//
+//  Created by playhong on 2024/01/26.
+//  Copyright © 2024 Zipthing. All rights reserved.
+//
+
+import Alamofire
+
+public enum NetworkProvider {
+
+}
