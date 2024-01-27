96d3e8cb2bfa95e21b9a0c68a73f1a2bc028b1ed
seongwon <tjddnjs549@gmail.com>
2024-01-28 01:28:45 +0900

    feat: UserDefaultsProperties.swift (유저디폴트 키,값 저장)

---
+++ Projects/Shared/DesignSystem/Sources/UserDefaultsProperties.swift
@@ -0,0 +1,29 @@
+//
+//  UserDefaultsProperties.swift
+//  DesignSystem
+//
+//  Created by 박성원 on 1/28/24.
+//  Copyright © 2024 Zipthing. All rights reserved.
+//
+
+import Foundation
+
+
+public struct UserDefaultsProperties {
+    
+    
+//    ❗️예시
+    
+//    @UserDefault(key: "isSelected", defaultValue: false)
+//    static var isSelected: Bool
+    
+//    @UserDefault(key: "email", defaultValue: nil)
+//    static var email: String?
+
+}
+
+
+// ❗️다른값 저장 시
+// UserDefaultsProperties.isSelected = true
+
+// 자동으로 해당 프로퍼티가 가진 key값을 사용하여 UserDefaults에 할당한 값 저장
