67213125e6a27d9bad188244887f832c029e956c
Hongsik Choi <playhong16@gmail.com>
2024-01-28 21:59:29 +0900

    create: BaseAPI.swift

---
+++ Projects/Shared/DesignSystem/Sources/Network/BaseAPI.swift
@@ -0,0 +1,56 @@
+//
+//  BaseAPI.swift
+//  DesignSystem
+//
+//  Created by playhong on 2024/01/28.
+//  Copyright © 2024 Zipthing. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+public protocol BaseAPI: URLRequestConvertible {
+    var baseURL: URL { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: HTTPHeaders { get }
+    var parameters: Parameters? { get }
+    var encoding: ParameterEncoding { get }
+}
+
+extension BaseAPI {
+    public var baseURL: URL {
+        // Note: - 임시 URL 넣었습니다.
+        guard let url = URL(string: "zipthing.com") else { fatalError() }
+        return url
+    }
+    
+    public var headers: HTTPHeaders {
+        return [
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        ]
+    }
+    
+    public var encoding: ParameterEncoding {
+        switch self {
+        default:
+            return JSONEncoding.default
+        }
+    }
+    
+    public func asURLRequest() throws -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        var urlRequest = URLRequest(url: url)
+        
+        urlRequest.method = method
+        urlRequest.headers = headers
+        
+        if let parameters = parameters {
+            let encoding = JSONEncoding.default
+            return try encoding.encode(urlRequest, with: parameters)
+        }
+        
+        return urlRequest
+    }
+}
