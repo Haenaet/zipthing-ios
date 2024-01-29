44dd35bd6d4592091e6931e43f2964448c28151a
Hongsik Choi <playhong16@gmail.com>
2024-01-29 17:06:40 +0900

    feature: HTTP Request 메서드 구현

--- Projects/Shared/DesignSystem/Sources/Network/NetworkProvider.swift
+++ Projects/Shared/DesignSystem/Sources/Network/NetworkProvider.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import Alamofire
+
+public enum NetworkProvider {
+    public static func request<T: Decodable>(api: BaseAPI, to responseType: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        AF.request(api).responseDecodable(of: responseType) { response in
+            switch response.result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(_):
+                guard let statusCode = response.response?.statusCode else {
+                    print("Network ERROR: Unknown Error")
+                    return completion(.failure(.unknownError))
+                }
+                
+                if (400..<500).contains(statusCode) {
+                    print("Network ERROR (statusCode:\(statusCode)): Client Error!")
+                    completion(.failure(.clientError(statusCode: statusCode)))
+                }
+                
+                if (500..<600).contains(statusCode) {
+                    print("Network ERROR (statusCode:\(statusCode)): Server Error!")
+                    completion(.failure(.serverError(statusCode: statusCode)))
+                }
+            }
+        }
+    }
+}
