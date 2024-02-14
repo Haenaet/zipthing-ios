7ffe3c5bafde31b7f0b02422ea82bf979681e5b8
seryeong <seryz029@gmail.com>
2024-02-14 15:46:12 +0900

    design: 내 정보 페이지 UI작업

--- Projects/Features/FamilyFeature/FamilyInfoEditViewController.swift
+++ Projects/Features/FamilyFeature/FamilyInfoEditViewController.swift
@@ -7,10 +7,167 @@
 //
 
 import UIKit
+import DesignSystem
 
-class FamilyInfoEditViewController: UIViewController {
+class FamilyInfoEditViewController: BaseUIViewController {
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("취소", for: .normal)
+        button.titleLabel?.font = UIFont().pretendard(ofSize: 20, weight: .medium)
+        button.tintColor = .black
+        
+        return button
+    }()
+    
+    private lazy var editButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("저장", for: .normal)
+        button.titleLabel?.font = UIFont().pretendard(ofSize: 20, weight: .extraBold)
+        button.tintColor = .black
+        
+        return button
+    }()
+    
+    private var myInformationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "내 정보"
+        label.font = UIFont().pretendard(ofSize: 20, weight: .bold)
+        
+        return label
+    }()
+    
+    private var nicknameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "별명"
+        label.font = UIFont().pretendard(ofSize: 16, weight: .extraBold)
+        
+        return label
+    }()
+    
+    private var nicknameTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "ex. 똥강아지"
+        textfield.borderStyle = .roundedRect
+        
+        return textfield
+    }()
+    
+    private var phoneNumberLabel: UILabel = {
+        let label = UILabel()
+        label.text = "휴대폰 번호"
+        label.font = UIFont().pretendard(ofSize: 16, weight: .extraBold)
+        
+        return label
+    }()
+    
+    private var phoneNumberTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "ex. 010-1234-5678"
+        textfield.borderStyle = .roundedRect
+        
+        return textfield
+    }()
+    
+    private var birthdayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "생일"
+        label.font = UIFont().pretendard(ofSize: 16, weight: .extraBold)
+        
+        return label
+    }()
+    
+    private var datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .compact
+        
+        return datePicker
+    }()
+    
+    private var myCharacterLabel: UILabel = {
+        let label = UILabel()
+        label.text = "내 캐릭터"
+        label.font = UIFont().pretendard(ofSize: 16, weight: .extraBold)
+        
+        return label
+    }()
+    
+    private var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        
+        
+        return scrollView
+    }()
     
     override func viewDidLoad() {
-           view.backgroundColor = .lightGray
-       }
+        view.backgroundColor = DesignSystemAsset.yellow050.color
+        setUI()
+        setLayout()
+    }
+    
+    override func setUI() {
+        view.addSubview(scrollView)
+        scrollView.addSubviews(cancelButton, editButton, myInformationLabel, nicknameLabel, nicknameTextField, phoneNumberLabel, phoneNumberTextField, birthdayLabel, datePicker, myCharacterLabel)
+    }
+    
+    override func setLayout() {
+        scrollView.snp.makeConstraints {
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.top.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        cancelButton.snp.makeConstraints {
+            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(22)
+        }
+        
+        editButton.snp.makeConstraints {
+            $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-16)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(22)
+        }
+        
+        myInformationLabel.snp.makeConstraints {
+            $0.leading.equalTo(cancelButton.snp.trailing).offset(117)
+            $0.trailing.equalTo(editButton.snp.leading).offset(-117)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(22)
+        }
+        
+        nicknameLabel.snp.makeConstraints {
+            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+            $0.top.equalTo(cancelButton.snp.bottom).offset(31)
+        }
+        
+        nicknameTextField.snp.makeConstraints {
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.top.equalTo(nicknameLabel.snp.bottom).offset(7)
+            $0.height.equalTo(50)
+        }
+        
+        phoneNumberLabel.snp.makeConstraints {
+            $0.top.equalTo(nicknameTextField.snp.bottom).offset(28)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
+        
+        phoneNumberTextField.snp.makeConstraints {
+            $0.top.equalTo(phoneNumberLabel.snp.bottom).offset(7)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.height.equalTo(50)
+        }
+        
+        birthdayLabel.snp.makeConstraints {
+            $0.top.equalTo(phoneNumberTextField.snp.bottom).offset(28)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
+        
+        datePicker.snp.makeConstraints {
+            $0.top.equalTo(birthdayLabel.snp.bottom).offset(7)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
+        
+        myCharacterLabel.snp.makeConstraints {
+            $0.top.equalTo(datePicker.snp.bottom).offset(28)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
+    }
 }
