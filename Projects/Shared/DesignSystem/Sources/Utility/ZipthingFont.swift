76210fb23330d8ea5bd8c110436ba8f1d39cb4cc
Hongsik Choi <playhong16@gmail.com>
2024-02-18 23:47:27 +0900

    style: 접근 제어자 추가

--- Projects/Shared/DesignSystem/Sources/Utility/ZipthingFont.swift
+++ Projects/Shared/DesignSystem/Sources/Utility/ZipthingFont.swift
@@ -8,27 +8,27 @@
 
 import Foundation
 
-enum ZipthingFont {
+public enum ZipthingFont {
     // MARK: - Title
 
-    static let title2 = DesignSystemFontFamily.Pretendard.regular.font(size: 33)
-    static let title4 = DesignSystemFontFamily.Pretendard.regular.font(size: 30)
-    static let title6 = DesignSystemFontFamily.Pretendard.bold.font(size: 20)
-    static let title8 = DesignSystemFontFamily.Pretendard.medium.font(size: 20)
+    public static let title2 = DesignSystemFontFamily.Pretendard.regular.font(size: 33)
+    public static let title4 = DesignSystemFontFamily.Pretendard.regular.font(size: 30)
+    public static let title6 = DesignSystemFontFamily.Pretendard.bold.font(size: 20)
+    public static let title8 = DesignSystemFontFamily.Pretendard.medium.font(size: 20)
     
     // MARK: - Sub Title
 
-    static let subTitle2 = DesignSystemFontFamily.Pretendard.bold.font(size: 19)
-    static let subTitle3 = DesignSystemFontFamily.Pretendard.bold.font(size: 16)
-    static let subTitle4 = DesignSystemFontFamily.Pretendard.bold.font(size: 14)
-    static let subTitle6 = DesignSystemFontFamily.Pretendard.bold.font(size: 12)
+    public static let subTitle2 = DesignSystemFontFamily.Pretendard.bold.font(size: 19)
+    public static let subTitle3 = DesignSystemFontFamily.Pretendard.bold.font(size: 16)
+    public static let subTitle4 = DesignSystemFontFamily.Pretendard.bold.font(size: 14)
+    public static let subTitle6 = DesignSystemFontFamily.Pretendard.bold.font(size: 12)
     
     // MARK: - Body
 
-    static let body2 = DesignSystemFontFamily.Pretendard.light.font(size: 22)
-    static let body4 = DesignSystemFontFamily.Pretendard.regular.font(size: 17)
-    static let body6 = DesignSystemFontFamily.Pretendard.medium.font(size: 16)
-    static let body8 = DesignSystemFontFamily.Pretendard.regular.font(size: 14)
-    static let caption2 = DesignSystemFontFamily.Pretendard.bold.font(size: 12)
-    static let caption4 = DesignSystemFontFamily.Pretendard.medium.font(size: 12)
+    public static let body2 = DesignSystemFontFamily.Pretendard.light.font(size: 22)
+    public static let body4 = DesignSystemFontFamily.Pretendard.regular.font(size: 17)
+    public static let body6 = DesignSystemFontFamily.Pretendard.medium.font(size: 16)
+    public static let body8 = DesignSystemFontFamily.Pretendard.regular.font(size: 14)
+    public static let caption2 = DesignSystemFontFamily.Pretendard.bold.font(size: 12)
+    public static let caption4 = DesignSystemFontFamily.Pretendard.medium.font(size: 12)
 }
